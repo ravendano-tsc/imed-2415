690ec9c3ef6e87c4f1b50455d90bf775ca40d326
Your Name <you@example.com>
2025-02-09 20:16:54 -0800

    commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html>
+<head><title>My First PHP File</title></head>
+
+<body>
+<center><h1>Welcome my Personal Home Page</h1></center>
+<?PHP
+
+echo "<p>Hello World</p>";
+print "<p>This is written in PHP!</p>";
+
+?>
+</body>
+</html>
